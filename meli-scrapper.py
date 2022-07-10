9c0b806ef9732e14ec7b1c7477bb9248b5eb231f
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-10 01:01:24 -0300

    agrego seguimiento de páginas

--- meli-scrapper.py
+++ meli-scrapper.py
@@ -1,29 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 
-driver = webdriver.Chrome("/opt/homebrew/bin/chromedriver")
+chromeoptions=Options()
+chromeoptions.add_argument("--headless")
+driver = webdriver.Chrome("/opt/homebrew/bin/chromedriver", chrome_options=chromeoptions)
 products=[]
 prices=[]
 links=[]
-driver.get("https://listado.mercadolibre.com.ar/repuestos/407-coupe")
-#driver.get("https://listado.mercadolibre.com.ar/407-coupe_PublishedToday_YES")
+melipagestep=50
 
-content = driver.page_source
-soup = BeautifulSoup(content, 'html.parser')
+url="https://listado.mercadolibre.com.ar/repuestos/407"
 
-for a in soup.find_all('div', class_="ui-search-result__content-wrapper"):
-    name=a.find('a', href=True, class_="ui-search-item__group__element ui-search-link")
-    link=name['href']
-    name=a.find('h2', class_="ui-search-item__title")
-    price=a.find('span', class_="price-tag-fraction")
+
+for i in range(1, 2000, melipagestep):
+
+    meliurl=url + "_Desde_" + str(i) + "_NoIndex_True"
+    print(meliurl)
+    driver.get(url)
+    content = driver.page_source
+    soup = BeautifulSoup(content, 'html.parser')
+
+    for a in soup.find_all('div', class_="ui-search-result__content-wrapper"):
+        name=a.find('a', href=True, class_="ui-search-item__group__element ui-search-link")
+        link=name['href']
+        name=a.find('h2', class_="ui-search-item__title")
+        price=a.find('span', class_="price-tag-fraction")
     
-    print(link)
-    
-    products.append(name.text)
-    prices.append(price.text)
-    links+= [link]
+        products.append(name.text)
+        prices.append(price.text)
+        links+= [link]
+
 dateandtime = datetime.datetime.now()
 excelfilename = "/Users/andresabastante/resultados-excel/resultados repuestos 407 coupe " + dateandtime.strftime("%Y-%m-%d %H:%M:%S")+".csv"
 df = pd.DataFrame({'Producto':products, 'Precio':prices, 'Link':links}) 
