b6f4fab4062aa38f8bdfec2806c019e3f6e3ad0c
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-10 00:00:09 -0300

    asdasd

--- meli-scrapper.py
+++ meli-scrapper.py
@@ -4,9 +4,9 @@ import pandas as pd
 import datetime
 
 driver = webdriver.Chrome("/opt/homebrew/bin/chromedriver")
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product 
+products=[]
+prices=[]
+links=[]
 driver.get("https://listado.mercadolibre.com.ar/repuestos/407-coupe")
 #driver.get("https://listado.mercadolibre.com.ar/407-coupe_PublishedToday_YES")
 
@@ -14,12 +14,17 @@ content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 
 for a in soup.find_all('div', class_="ui-search-result__content-wrapper"):
+    name=a.find('a', href=True, class_="ui-search-item__group__element ui-search-link")
+    link=name['href']
     name=a.find('h2', class_="ui-search-item__title")
-    price=a.find('span', class_="price-tag-text-sr-only")
+    price=a.find('span', class_="price-tag-fraction")
+    
+    print(link)
+    
     products.append(name.text)
     prices.append(price.text)
-
+    links+= [link]
 dateandtime = datetime.datetime.now()
 excelfilename = "/Users/andresabastante/resultados-excel/resultados repuestos 407 coupe " + dateandtime.strftime("%Y-%m-%d %H:%M:%S")+".csv"
-df = pd.DataFrame({'Producto':products, 'Precio':prices}) 
+df = pd.DataFrame({'Producto':products, 'Precio':prices, 'Link':links}) 
 df.to_csv(excelfilename, index=False, encoding='utf-8')
