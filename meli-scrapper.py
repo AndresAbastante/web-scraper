95b99fa188477e9459e222ac1e6a9746a25b5198
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-11 21:51:32 -0300

    asd

--- meli-scrapper.py
+++ meli-scrapper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 import subprocess
+from tqdm import tqdm
 
 chromeoptions=Options()
 chromeoptions.add_argument("--headless")
@@ -12,10 +13,12 @@ products=[]
 prices=[]
 links=[]
 melipagestep=50
+
 search=input("Input MeLi search")
 url="https://listado.mercadolibre.com.ar/" + search
+maxfinds=int(input("Input max desired findings"))
 
-for i in range(1, 10000, melipagestep):
+for i in tqdm (range (1, maxfinds, melipagestep), desc="Fetching..."):
 
     meliurl=url + "_Desde_" + str(i) + "_NoIndex_True"
     driver.get(url)
@@ -26,7 +29,6 @@ for i in range(1, 10000, melipagestep):
     #is currently being used.
     
     formatcheck=soup.find('a', class_="ui-search-result__content ui-search-link")
-    print(formatcheck)
     if formatcheck==None:
         for tag in soup.find_all('div', class_="ui-search-result__wrapper"):
             name=tag.find('h2', class_="ui-search-item__title")
@@ -49,6 +51,6 @@ for i in range(1, 10000, melipagestep):
 
 dateandtime = datetime.datetime.now()
 excelfilename = "/Users/andresabastante/resultados-excel/results " + search + " " + dateandtime.strftime("%Y-%m-%d %H:%M:%S")+".csv"
-df = pd.DataFrame({'Producto':products, 'Precio':prices, 'Link':links}) 
+df = pd.DataFrame({'Producto':products, 'Precio':prices, 'Link':links})
 df.to_csv(excelfilename, index=False, encoding='utf-8')
 subprocess.call(['open', excelfilename])
