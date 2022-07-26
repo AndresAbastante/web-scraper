0dad3bb772492ddc70858ebf44ee8f1312698a07
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-26 16:04:37 -0300

    asd

--- pirulo-scrapper-and-notifier.py
+++ pirulo-scrapper-and-notifier.py
@@ -20,11 +20,11 @@ condition=[]
 stocks=[]
 sets=[]
 maxpages=50
-url='https://starcitygames.com/search/?sort=pricedesc&hawksearchable=search_includetext%3A%20%22default%22&instockonly=Yes&language=Japanese&mpp=96&pg='
+url='https://mtgpirulo.crystalcommerce.com/advanced_search?commit=Buscar&page='
 print('Scrapping MTGPirulo Japanese cards')
 
 for j in tqdm (range (1, maxpages), desc='Scrapping...'):
-    finalurl=url + str(j)
+    finalurl=url + str(j) + '&search%5Bbuy_price_gte%5D=&search%5Bbuy_price_lte%5D=&search%5Bcatalog_group_id_eq%5D=&search%5Bcategory_ids_with_descendants%5D%5B%5D=&search%5Bcategory_ids_with_descendants%5D%5B%5D=8&search%5Bdirection%5D=descend&search%5Bfuzzy_search%5D=&search%5Bin_stock%5D=1&search%5Bsell_price_gte%5D=&search%5Bsell_price_lte%5D=&search%5Bsort%5D=sell_price&search%5Btags_name_eq%5D=&search%5Bvariants_with_identifier%5D%5B14%5D%5B%5D=&search%5Bvariants_with_identifier%5D%5B15%5D%5B%5D=&search%5Bvariants_with_identifier%5D%5B15%5D%5B%5D=Japanese&search%5Bwith_descriptor_values%5D%5B10%5D=&search%5Bwith_descriptor_values%5D%5B11%5D=&search%5Bwith_descriptor_values%5D%5B1259%5D=&search%5Bwith_descriptor_values%5D%5B13%5D=&search%5Bwith_descriptor_values%5D%5B17023%5D=&search%5Bwith_descriptor_values%5D%5B348%5D=&search%5Bwith_descriptor_values%5D%5B361%5D=&search%5Bwith_descriptor_values%5D%5B6%5D=&search%5Bwith_descriptor_values%5D%5B7%5D=&search%5Bwith_descriptor_values%5D%5B9%5D=&utf8=%E2%9C%93'
     driver.get(finalurl)
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
