550abffa9c7fbb66a3b77132fca1b264622a6b56
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-26 16:22:18 -0300

    asd

--- scg-scrapper.py
+++ scg-scrapper.py
@@ -10,8 +10,8 @@ import subprocess
 from tqdm import tqdm
 
 chromeoptions=Options()
-#chromeoptions.add_argument('--headless')
-#chromeoptions.add_argument('--silent')
+chromeoptions.add_argument('--headless')
+chromeoptions.add_argument('--silent')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chromeoptions)
 dateandtime = datetime.datetime.now()
 products=[]
@@ -31,34 +31,18 @@ for j in tqdm (range (1, maxpages), desc='Scrapping...'):
     soup = BeautifulSoup(content, 'html.parser')
 
     for variants in soup.find_all('div', class_='hawk-results-item'):
-        print(variants)
         name=variants.find('div', re.compile('_item_lnk2'))
         set=variants.find('div', re.compile('item_setLink'))
-        link=set['href']
+        #link=set['href']
         condition=variants.find('div', class_='hawk-results-item__options-table-cell hawk-results-item__options-table-cell--name childCondition')
         price=variants.find('div', class_='hawk-results-item__options-table-cell hawk-results-item__options-table-cell--price childAttributes')
         stock=variants.find('div', class_='hawk-results-item__options-table-cell hawk-results-item__options-table-cell--qty childAttributes')
-        print(name)
-        print(set)
-        print(link)
-        print(condition)
-        print(price)
-        print(stock)
         conditions+=[condition]
         products+=[name]
         prices+=[price]
         sets+=[set]
         stocks+=[stock]
-        links+=[link]
+        #links+=[link]
 
-#olddf = pd.read_csv('mtgpirulo-japanese-stock.csv')
-#csvfilename = 'mtgpirulo-new-results' + dateandtime.strftime('%y-%m-%d %H:%M') + '.csv'
-newdf = pd.DataFrame({'Product':products, 'Price':prices, 'Condition':conditions, 'Set':sets, 'Stock':stocks, 'Link':links})
+newdf = pd.DataFrame({'Product':products, 'Price':prices, 'Condition':conditions, 'Set':sets, 'Stock':stocks})
 newdf.to_csv('scg-japanese-stock.csv')
-#newdf=pd.read_csv(csvfilename)
-
-#mergeddf.to_csv('merged' + dateandtime.strftime('%y-%m-%d %H:%M') + '.csv')
-
-#subprocess.call(['open', 'merged.csv'])
-#if df.empty==False:
-#    subprocess.call(['notify', '-bulk', '-i', excelfilename])
