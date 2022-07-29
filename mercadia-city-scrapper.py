790140e13e4f0a9b4c55814c0b07ccb7f36d26b3
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-29 11:02:23 -0300

    asd

--- mercadia-city-scrapper.py
+++ mercadia-city-scrapper.py
@@ -21,30 +21,32 @@ links=[]
 conditions=[]
 stocks=[]
 sets=[]
-maxpages=20
-url='https://starcitygames.com/search/?sort=pricedesc&hawksearchable=search_includetext%3A%20%22default%22&instockonly=Yes&language=Japanese&mpp=96&pg='
-print('Scrapping SCG Japanese stock')
+#maxpages=20
+#file = open('meli-urls.txt','r')
 
-for j in tqdm (range (1, maxpages), desc='Scrapping...'):
-    finalurl=url + str(j)
-    driver.get(finalurl)
-    content = driver.page_source
-    soup = BeautifulSoup(content, 'html.parser')
+print('Scrapping MercadiaCity Japanese stock')
 
-    for variants in soup.find_all('div', class_='product details product-item-details'):
-        name=variants.find('a', href=True, class_=('product-item-link'))
-        link=name['href']
-        condition=variants.find('div', class_='product description product-item-description')
-        price=variants.find('span', class_='price')
-        stockfilter=variants.find('div', class_='aw-cus__customstockstatus')
-        stock=(stockfilter.find('div', class_='text').text)[6:]
-        
-        conditions.append(condition)
-        products.append(name.text)
-        prices.append(price.text)
-        sets.append(set.text)
-        stocks.append(stock)
-        links+=[link]
+urlnumber = sum(1 for line in open('mercadia-city-urls.txt','r'))
+with open('mercadia-city-urls.txt','r') as f:
+    for url in tqdm(f, total=urlnumber):
+        print(url)
+        driver.get(url)
+        content = driver.page_source
+        soup = BeautifulSoup(content, 'html.parser')
 
-newdf = pd.DataFrame({'Product':products, 'Set':sets, 'Condition':conditions, 'Price':prices, 'Stock':stocks, 'Link':links})
+        for variants in soup.find_all('div', class_='product details product-item-details'):
+            name=variants.find('a', href=True, class_=('product-item-link'))
+            link=name['href']
+            name=(name.text)[9:-7]
+            condition=(variants.find('div', class_='product description product-item-description').text)[8:-6]
+            price=variants.find('span', class_='price')
+            stockfilter=(variants.find('div', class_='aw-cus__customstockstatus').text)
+
+            conditions.append(condition)
+            products.append(name)
+            prices.append(price.text)
+            stocks.append(stockfilter)
+            links+=[link]
+
+newdf = pd.DataFrame({'Product':products, 'Condition':conditions, 'Price':prices, 'Stock':stocks, 'Link':links})
 newdf.to_csv('Mercadia-City-japanese-stock.csv')
