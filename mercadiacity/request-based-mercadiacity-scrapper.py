d9b09913d7782e8234662d0f25194c8b4c412d47
AndresAbastante <atabastante@gmail.com>
2022-09-15 19:22:00 -0300

    asd

--- mercadiacity/request-based-mercadiacity-scrapper.py
+++ mercadiacity/request-based-mercadiacity-scrapper.py
@@ -4,8 +4,6 @@ import pandas as pd
 from tqdm import tqdm
 import time
 
-print('Scrapping MercadiaCity Japanese stock')
-
 elapsedseconds = time.time()
 requestheaders = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 products=[]
@@ -14,12 +12,12 @@ links=[]
 conditions=[]
 stocks=[]
 sets=[]
-maxpages=100
+maxpages=25
 urlnumber = sum(1 for line in open('mercadia-city-urls.txt','r'))
+print('Scrapping MercadiaCity Japanese stock')
 
 with open('mercadia-city-urls.txt','r') as f:
     for url in tqdm(f, total=urlnumber, desc='Scrapping sites...'):
-        
         for page in tqdm (range (1, maxpages), desc='Scrapping pages... '):
             finalurl= url + '&p=' + str(page)
             response = (requests.get(finalurl, requestheaders)).text
@@ -32,7 +30,6 @@ with open('mercadia-city-urls.txt','r') as f:
                     name=(name.text)[9:-7]
                     condition=(variants.find('div', class_='product description product-item-description').text)[8:-6]
                     price=(variants.find('span', class_='price').text)
-                    
                     conditions.append(condition)
                     products.append(name)
                     prices.append(price)
@@ -40,7 +37,6 @@ with open('mercadia-city-urls.txt','r') as f:
             else:
                 print('No more pages to iterate!')
                 break
-
 newdf = pd.DataFrame({'Product':products, 'Price':prices, 'Condition':conditions, 'Link':links})
 newdf.to_csv('Mercadia-City-japanese-stock.csv', index=False)
 elapsedseconds = time.time() - elapsedseconds
