3cba6de0f4567c5ed913dca68deb8d562413631f
AndresAbastante <atabastante@gmail.com>
2023-07-04 11:16:35 -0300

    fixing meli new tags

--- mercadolibre/meli-scraper.py
+++ mercadolibre/meli-scraper.py
@@ -7,7 +7,6 @@ import subprocess
 from os.path import exists
 import os
 
-#pd.options.mode.chained_assignment = None
 requestheaders={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 products=[]
 prices=[]
@@ -15,8 +14,8 @@ links=[]
 meliitemsstep=48
 urlstxtfile='meli-urls.txt'
 urlnumber=sum(1 for line in open(urlstxtfile,'r'))
-#maxitems=200
 maxitems=48
+pricefilter=3500
 
 def html_response_into_soup(url,requestheaders):
 	response=(requests.get(url, requestheaders)).text
@@ -28,34 +27,50 @@ with open(urlstxtfile,'r') as f:
 		print('Scraping ' + url)
 		noresultscheck=html_response_into_soup(url,requestheaders).find('div', class_='ui-search-rescue__info')
 		if noresultscheck==None:
-			variants=None
-			for variants in html_response_into_soup(url,requestheaders).find_all('a', href=True, class_='promotion-item__link-container'):
-				name=variants.find('p', class_='promotion-item__title')
-				price=variants.find('span', class_='andes-money-amount__fraction')
-				if int(float(price.text.replace(".", ""))) > 5000:
-					link=variants['href'].split('#',1)[0]
-					products.append(name.text)
-					prices.append(f'${price.text}')
-					links += [link]
-			if variants==None:
-				#singlepagecheck=html_response_into_soup(url,requestheaders).find('li', class_='andes-pagination__page-count')
-				#if singlepagecheck==None:
-				#	maxitems=48
-				for pages in tqdm (range (1, maxitems, meliitemsstep), desc='Scraping pages... '):
-					newurl=url + '_Desde_' + str(pages)
-					for variants in html_response_into_soup(newurl,requestheaders).find_all('div', class_='ui-search-result__wrapper'):
+			pagescheck=html_response_into_soup(url,requestheaders).find('li', class_='andes-pagination__page-count')
+			if pagescheck==None:
+					pageammount=1
+			else:
+					pageammount=int(pagescheck.text.split(" ")[1])
+			for page in tqdm (range (pageammount), desc='Scraping pages... '):
+				newurl=url + '_Desde_' + str(page * meliitemsstep)
+				variants=None
+				for variants in html_response_into_soup(newurl,requestheaders).find_all('div', class_='ui-search-result__wrapper shops__result-wrapper'):
+					print("caso A")
+					price=variants.find('span', class_='andes-money-amount__fraction')
+					print(int(float(price.text.replace(".", ""))))
+					if int(float(price.text.replace(".", ""))) > pricefilter:
 						name=variants.find('h2', class_='ui-search-item__title')
-						price=variants.find('span', class_='price-tag-fraction')
-						linkclass=variants.find('a', href=True, class_='ui-search-result__content ui-search-link')
-						if linkclass==None:
-							linkclass=variants.find('a', href=True, class_='ui-search-link')
-							if linkclass==None:
-								linkclass=variants.find('a', href=True, class_='ui-search-item__group__element ui-search-link')
+						linkclass=variants.find('a', href=True, class_='ui-search-link')
 						link=linkclass['href'].split('#',1)[0]
-						if int(float(price.text.replace(".", ""))) > 5000:
+						products.append(name.text)
+						prices.append(f'${price.text}')
+						links += [link]
+				if variants==None:
+					for variants in html_response_into_soup(newurl,requestheaders).find_all('a', href=True, class_='promotion-item__link-container'):
+						print("caso B")
+						name=variants.find('p', class_='promotion-item__title')
+						price=variants.find('span', class_='andes-money-amount__fraction')
+						if int(float(price.text.replace(".", ""))) > pricefilter:
+							link=variants['href'].split('#',1)[0]
 							products.append(name.text)
 							prices.append(f'${price.text}')
 							links += [link]
+						if variants==None:
+							for variants in html_response_into_soup(newurl,requestheaders).find_all('div', class_='ui-search-result__wrapper'):
+								print("caso C")
+								name=variants.find('h2', class_='ui-search-item__title')
+								price=variants.find('span', class_='price-tag-fraction')
+								linkclass=variants.find('a', href=True, class_='ui-search-result__content ui-search-link')
+								if linkclass==None:
+									linkclass=variants.find('a', href=True, class_='ui-search-link')
+									if linkclass==None:
+										linkclass=variants.find('a', href=True, class_='ui-search-item__group__element ui-search-link')
+								link=linkclass['href'].split('#',1)[0]
+								if int(float(price.text.replace(".", ""))) > pricefilter:
+									products.append(name.text)
+									prices.append(f'${price.text}')
+									links += [link]
 			filename=url.replace('https://','').replace('?','').replace('/','-').replace('.','').replace('www','').replace('com','').replace('ar','').replace('=','').replace('_','-').replace('&','').replace('=','')
 			filename = f"{filename[:45]}.csv"
 			tempdf=pd.DataFrame({'Product':products, 'Price':prices, 'Link':links})
