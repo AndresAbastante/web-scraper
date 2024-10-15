5a62b9f17ea8d0dc1c565e1bf1732d1055dcf1c5
andresabastante <atabastante@gmail.com>
2024-10-15 16:05:54 -0300

    more changes

--- mercadolibre/daily-metrics/scraper.py
+++ mercadolibre/daily-metrics/scraper.py
@@ -23,78 +23,86 @@ urlnumber=sum(1 for line in open(urlstxtfile,'r'))
 maxitems=48
 link="https://autos.mercadolibre.com.ar/dueno-directo/_OrderId_PRICE_PublishedToday_YES_NoIndex_True"
 oldcsv="old_links.csv"
+highlightscsv="highlights.csv"
 
 def html_response_into_soup(link):
-	response=(requests.get(link, headers)).text
-	soup=BeautifulSoup(response, 'html.parser')
-	return soup
+  response=(requests.get(link, headers)).text
+  soup=BeautifulSoup(response, 'html.parser')
+  return soup
 
 def link_gather(link):
- soup=html_response_into_soup(link)
- print ('Scraping ' + link)
- for variants in soup.find_all('div', class_='ui-search-result__wrapper'):
-  linkclass=variants.find('a', href=True, class_='')
-  link=linkclass['href'].split('-_JM#',1)[0]
-  links.append(link)
- siguiente=soup.find('a', class_='andes-pagination__link', title="Siguiente")
- if siguiente:
   try:
-   link=siguiente['href']
+    soup=html_response_into_soup(link)
   except ValueError:
-   logging.info ("puto")
+    logging.info ("puto")
+  print ('Scraping ' + link)
+  for variants in soup.find_all('div', class_='ui-search-result__wrapper'):
+    linkclass=variants.find('a', href=True, class_='')
+    link=linkclass['href'].split('-_JM#',1)[0]
+    links.append(link)
+  siguiente=soup.find('a', class_='andes-pagination__link', title="Siguiente")
+  if siguiente:
+    try:
+      link=siguiente['href']
+    except ValueError:
+      logging.info ("puto")
   return(link)
 
 async def fetch(session, link):
  async with session.get(link) as response:
   return await response.text()
 
-def scraper(link):
- soup=html_response_into_soup(link)
- header=soup.find('div', class_='ui-pdp-container__col col-2 ui-vip-core-container--short-description ui-vip-core-container--column__right')
- yearkms=header.find('span', class_='ui-pdp-subtitle')
- title=header.find('h1', class_='ui-pdp-title').text
- pattern=re.compile(r'(\d{4})\s*\|\s*([\d\.]+)\s*km')
- match=pattern.search(str(yearkms))
- if match and title:
-  year=match.group(1)
-  kms=match.group(2)
- else:
-  logging.error ('No match for year and kms. Those will be set as "Null"')
-  year='Null'
-  kms='Null'
- return title, year, kms
+async def scraper(resp):
+  try:
+    soup = BeautifulSoup(await resp.text(), 'html.parser')
+    header=soup.find('div', class_='ui-pdp-container__col col-2 ui-vip-core-container--short-description ui-vip-core-container--column__right')
+    yearkms=header.find('span', class_='ui-pdp-subtitle')
+    title=header.find('h1', class_='ui-pdp-title').text
+    pattern=re.compile(r'(\d{4})\s*\|\s*([\d\.]+)\s*km')
+    match=pattern.search(str(yearkms))
+    if match and title:
+      year=match.group(1)
+      kms=match.group(2)
+    else:
+      logging.error ('No match for year and kms. Those will be set as "Null"')
+      year='Null'
+      kms='Null'
+  except UnboundLocalError:
+    year='Null'
+    kms='Null'
+    title='Null'
+  finally:
+    return title, year, kms
    
 async def main():
- link="https://autos.mercadolibre.com.ar/dueno-directo/_Desde_1100_OrderId_PRICE_PublishedToday_YES_NoIndex_True"
- while link!= '':
-  link=link_gather(link)
- print(f"Found a total of {len(links)} results")
- newdf=pd.DataFrame({'links':links})
- if exists(oldcsv):
-  olddf=pd.read_csv(oldcsv, dtype={'Links':'string'})
-  mergeddf=olddf.merge(newdf, how='right', indicator='Exists')
-  highlights=mergeddf.query("Exists == 'right_only'")
-  if highlights.empty:
-   logging.info ("No new results found.")
+  link="https://autos.mercadolibre.com.ar/dueno-directo/_OrderId_PRICE_PublishedToday_YES_NoIndex_True"
+  while link!= '':
+    link=link_gather(link)
+  print(f"Found a total of {len(links)} results")
+  newlinksdf=pd.DataFrame({'links':links})
+  oldlinksdf=pd.read_csv(oldcsv, dtype={'Links':'string'})
+  mergeddf=oldlinksdf.merge(newlinksdf, how='right', indicator='Exists')
+  highlightslinksdf=mergeddf.query("Exists == 'right_only'")
+  if highlightslinksdf.empty:
+    logging.info ("No new results found.")
   else:
-   print(f'\n{" New items found! :D ":#^100}')
-   highlights.drop('Exists', axis=1, inplace=True)
-   highlights.to_csv(oldcsv, index=False)
- with open(oldcsv, newline='') as csvfile:
-  csvreader=csv.DictReader(csvfile)
-  for link in csvreader:
-    print(link['links'])
-    async with session.get(link['links']) as resp:
-      title, year, kms,  = await scraper(resp)
-      print(title)
-      titles.append(title)
-      years.append(year)
-      kmss.append(kms)
-      links2.append(link)
-    titles.append(title)
-    years.append(year)
-    kmss.append(kms)
-    links2.append(link)
-  resultsdf=pd.DataFrame({'title':titles, 'year':years, 'kmss':kmss})
-  resultsdf.to_csv('results.csv', index=False)
+    print(f"Found {len(highlightslinksdf)} new elements.")
+    highlightslinksdf.drop('Exists', axis=1, inplace=True)
+    session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
+    for index, row in highlightslinksdf.iterrows():
+      print(row['links'])
+      async with session.get(row['links']) as resp:
+        try:
+          title, year, kms,  = await scraper(resp)
+          titles.append(title)
+          years.append(year)
+          kmss.append(kms)
+          links2.append(row['links'])
+        except UnboundLocalError:
+          print("puto")
+    mergeddf.drop('Exists', axis=1, inplace=True)
+    newlinksdf.to_csv(oldcsv, index=False)
+    highlightsdf=pd.DataFrame({'title':titles, 'year':years, 'kms':kmss, 'links':links2})
+    highlightsdf.to_csv(highlightscsv, index=False)
+    
 asyncio.run(main())
