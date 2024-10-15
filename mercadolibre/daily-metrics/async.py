98a6a8a16ed0e23f11a01e7058b876590e717247
andresabastante <atabastante@gmail.com>
2024-10-15 13:41:22 -0300

    adding async

--- mercadolibre/daily-metrics/async.py
+++ mercadolibre/daily-metrics/async.py
@@ -1,22 +1,45 @@
+import aiohttp
 import asyncio
+import csv
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
 
-# Define an async function
-async def task(name, delay):
-    print(f"Task {name} started")
-    await asyncio.sleep(delay)  # Simulate a non-blocking delay
-    print(f"Task {name} finished after {delay} seconds")
+titles=[]
+years=[]
+kmss=[]
+links2=[]
 
-# Define the main async function
+async def scraper(resp):
+ soup = BeautifulSoup(await resp.text(), 'html.parser')
+ header=soup.find('div', class_='ui-pdp-container__col col-2 ui-vip-core-container--short-description ui-vip-core-container--column__right')
+ yearkms=header.find('span', class_='ui-pdp-subtitle')
+ title=header.find('h1', class_='ui-pdp-title').text
+ pattern=re.compile(r'(\d{4})\s*\|\s*([\d\.]+)\s*km')
+ match=pattern.search(str(yearkms))
+ if match and title:
+  year=match.group(1)
+  kms=match.group(2)
+ else:
+  logging.error ('No match for year and kms. Those will be set as "Null"')
+  year='Null'
+  kms='Null'
+ return title, year, kms
+
 async def main():
-    # Create multiple async tasks
-    tasks = [
-        task("A", 2),
-        task("B", 3),
-        task("C", 1)
-    ]
-    
-    # Use asyncio.gather to await all tasks to finish
-    await asyncio.gather(*tasks)
+    async with aiohttp.ClientSession() as session:
+        session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) 
+        with open('old_links.csv', newline='') as csvfile:
+            csvreader=csv.DictReader(csvfile)
+            for link in csvreader:
+                async with session.get(link['links']) as resp:
+                    title, year, kms,  = await scraper(resp)
+                    print(title)
+                    titles.append(title)
+                    years.append(year)
+                    kmss.append(kms)
+                    links2.append(link)
+            resultsdf=pd.DataFrame({'title':titles, 'year':years, 'kmss':kmss})
+            resultsdf.to_csv('results.csv', index=False)
 
-# Run the main function
 asyncio.run(main())
